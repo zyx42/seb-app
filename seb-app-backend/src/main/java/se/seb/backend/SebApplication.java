441ea07278711b452a423cc8dd088808d8e6caef
zyx42 <zyx42@dismail.de>
2020-11-20 16:53:41 +0200

    initial commit

---
+++ seb-app-backend/src/main/java/se/seb/backend/SebApplication.java
@@ -0,0 +1,12 @@
+package se.seb.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SebApplication.class, args);
+    }
+}
