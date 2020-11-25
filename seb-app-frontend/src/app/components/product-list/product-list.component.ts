c2e6308f7c437a240c94b2e3c525bab1cf3fbd94
zyx42 <zyx42@dismail.de>
2020-11-25 21:25:29 +0200

    set up styles for the front-end client

--- seb-app-frontend/src/app/components/product-list/product-list.component.ts
+++ seb-app-frontend/src/app/components/product-list/product-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {of} from 'rxjs';
 
 @Component({
   selector: 'app-product-list',
@@ -9,16 +11,24 @@ import { ProductService } from '../../services/product.service';
 export class ProductListComponent implements OnInit {
 
   products: any;
-  ageBracket = '';
-  incomeBracket = '';
-  student = false;
+  queryForm: FormGroup;
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,
+              private fb: FormBuilder) {
+    this.queryForm = this.fb.group({
+      ageBracket: ['', Validators.required],
+      incomeBracket: ['', Validators.required],
+      student: ['', Validators.required]
+    });
+  }
 
   ngOnInit(): void { }
 
   searchProductsByCriteria(): void {
-    this.productService.searchByCriteria(this.ageBracket, this.incomeBracket, this.student)
+    this.productService.searchByCriteria(
+      this.queryForm.value.ageBracket,
+      this.queryForm.value.incomeBracket,
+      this.queryForm.value.student)
       .subscribe(
           (products: any) => {
           this.products = products;
